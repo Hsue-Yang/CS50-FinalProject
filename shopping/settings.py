cfbe64fad4f8d674975516e80c0501157351eee5
HsuehRen <michael841218@gmail.com>
2025-06-24 10:50:00 +0800

    add whitenoise for production static files

--- shopping/settings.py
+++ shopping/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # For serving static files in production
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/' # load from root folder
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # collect static files to this folder
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shopping_app', 'static')] # collect static files from theis folder
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # For production use with WhiteNoise
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
